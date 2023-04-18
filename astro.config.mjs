b055c6b3da8b2d80f789c7b46765b124e63a5a95
新城 <12399806+createwj@user.noreply.gitee.com>
2023-04-18 17:02:42 +0800

    feat: add
    Signed-off-by: 新城 <12399806+createwj@user.noreply.gitee.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import Unocss from "@unocss/astro";
 // import { presetIcons, presetAttributify, presetUno } from 'unocss'
 // import unoConfig from './uno.config.js'
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     svelte(),
     Unocss({
