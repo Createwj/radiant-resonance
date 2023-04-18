207c3a47e8b067c133550fa9d6273559796a7932
新城 <12399806+createwj@user.noreply.gitee.com>
2023-04-18 18:18:47 +0800

    feat: add
    Signed-off-by: 新城 <12399806+createwj@user.noreply.gitee.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import svelte from "@astrojs/svelte";
 import Unocss from "@unocss/astro";
 // import { presetIcons, presetAttributify, presetUno } from 'unocss'
 // import unoConfig from './uno.config.js'
 
-
+import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     svelte(),
     Unocss({
